ddf74e257e97c75ec146510a70ff7a98d8d60689
ariful97768 <ariful97768@gmail.com>
2025-07-05 22:34:39 +0600

    Added modal

---
+++ src/Modal/Modal.jsx
@@ -0,0 +1,24 @@
+const Modal = ({ modalData }) => {
+
+    return (
+        <dialog id="my_modal_1" className="modal">
+            <div className="modal-box !p-0 max-w-max">
+                <button onClick={()=>document.getElementById('my_modal_1').close()} className="btn btn-sm btn-circle absolute right-3 top-3">✕</button>
+                <h3 className="font-semibold txt-secondary px-4 py-5 bg-[#F4F4F4]">{modalData.title}</h3>
+                <div className=" px-4 py-5">
+                    <div className={`grid ${modalData.col===2?"grid-cols-2":(modalData.col===3?"grid-cols-3":'grid-cols-1')} gap-4`}>
+                        {modalData?.data?.map(data => data)}
+                    </div>
+                    {/* if there is a button in form, it will close the modal */}
+                    <div className="modal-action">
+                        <form method="dialog">
+                            <button className="btn text-white bg-[#316EED]">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </dialog>
+    );
+};
+
+export default Modal;
